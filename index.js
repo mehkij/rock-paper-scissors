7a29ac2a4170f856ef883c993634b9479a6dab90
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-09-10 16:20:40 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// get a CPU's choice between the three options
+// get user input for their choice between the three options
+// if user's choice beats CPU's choice, output winner; else output loser
+// write code to handle draw
+// games goes up to 5 rounds
+// check for score; evaluate rounds won; if userScore > cpuScore, user wins; else CPU wins
