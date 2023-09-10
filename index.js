beb801d5273e40451f6aba871cd4bc2a7f8cfd83
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-09-10 16:59:26 -0400

    Add CPU choice function

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const battleChoices = ["Rock", "Paper", "Scissors"];
+
 // get a CPU's choice between the three options
+
+function getComputerChoice() {
+  const randomChoice = Math.floor(Math.random() * battleChoices.length);
+  return [randomChoice];
+}
+
+console.log(getComputerChoice(battleChoices));
+
+
 // get user input for their choice between the three options
 // if user's choice beats CPU's choice, output winner; else output loser
 // write code to handle draw
