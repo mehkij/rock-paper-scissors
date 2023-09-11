5ac4303a914dffc8a921a5b0bd8f2bd43e344bc9
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-09-11 09:54:38 -0400

    Add multiple rounds

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const battleChoices = ["rock", "paper", "scissors"];
+let userScore = 0;
+let cpuScore = 0;
 
 // get a CPU's choice between the three options
 
-function computerSelection() {
-  const randomChoice = Math.floor(Math.random() * battleChoices.length);
-  return [randomChoice];
-}
+const computerSelection = Math.floor(Math.random() * battleChoices.length);
 
 // get user input for their choice between the three options (make sure it is case insensitive)
 
@@ -15,22 +14,44 @@ const playerSelection = userInput.toLowerCase();
 // compare user choice to CPU choice
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "rock" && computerSelection() === 2) {
-    alert("You win! Rock beats scissors!");
-  } else if (playerSelection === "paper" && computerSelection() === 0) {
-    alert("You win! Paper beats rock!");
-  } else if (playerSelection === "scissors" && computerSelection() === 1) {
-    alert("You win! Scissors beats papaer!");
-  } else if (computerSelection() === 0 && playerSelection === "scissors") {
-    alert("You lose! Rock beats scissors!");
-  } else if (computerSelection() === 1 && playerSelection === "rock") {
-    alert("You lose! Paper beats rock!");
-  } else if (computerSelection() === 2 && playerSelection === "paper") {
-    alert("You lose! Scissors beats paper!");
+  if (playerSelection === "rock" && computerSelection === 2) {
+    console.log("You win! Rock beats scissors!");
+    userScore++;
+  } else if (playerSelection === "paper" && computerSelection === 0) {
+    console.log("You win! Paper beats rock!");
+    userScore++;
+  } else if (playerSelection === "scissors" && computerSelection === 1) {
+    console.log("You win! Scissors beats papaer!");
+    userScore++;
+  } else if (computerSelection === 0 && playerSelection === "scissors") {
+    console.log("You lose! Rock beats scissors!");
+    cpuScore++
+  } else if (computerSelection === 1 && playerSelection === "rock") {
+    console.log("You lose! Paper beats rock!");
+    cpuScore++
+  } else if (computerSelection === 2 && playerSelection === "paper") {
+    console.log("You lose! Scissors beats paper!");
+    cpuScore++
   } else {
-    alert("It was a draw!");
+    console.log("It was a draw!");
   }
 }
+
+// make game go up to 5 rounds
+
+const maxRounds = 5;
+let round = 0;
+
+function game() {
+  while (round < maxRounds) {
+    playRound();
+    round++;
+    console.log("Your score: " + userScore + "CPU Score: " + cpuScore)
+  }
+}
+
+console.log(game())
+
 
 // if user's choice beats CPU's choice, output winner; else output loser
 // write code to handle draw
